e95e1adca9924752c3923a0258ce78af0ec96f19
root <root@ip-172-31-44-70.ec2.internal>
2020-04-21 05:51:05 +0000

    1

---
+++ ec2.tf
@@ -0,0 +1,16 @@
+provider "aws" {
+  region                 = "us-east-1"
+  access_key             = "AKIA6BVISBY5NV5AGMP7"
+  secret_key             = "sc4n1wMBHWVtfjCEtxRZqzj4ihbFlz3YXse1QBaw"
+}
+resource "aws_instance" "example" {
+
+  ami                    = "ami-0affd4508a5d2481b"
+  instance_type          = "t2.micro"
+  key_name               = "malthumkar"
+  vpc_security_group_ids = ["Jenkins-SG"]
+
+  tags = {
+    name   = "terraform_instance"
+  }
+}
